c5c47f4204726f46aafbdaea1ee72cdf13c5e390
AndersonQueirooz <andersonqueiroz537@gmail.com>
2026-06-08 16:33:42 -0300

    Ajustes nas configurações

---
+++ src/main/java/queiroz/spring_security/controller/WelcomeController.java
@@ -0,0 +1,25 @@
+package queiroz.spring_security.controller;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+    @GetMapping
+    public String welcome(){
+        return "Welcome Spring-API";
+    }
+
+    @GetMapping("/users")
+    public String users(){
+        return "Authorized user";
+    }
+
+    @GetMapping("/managers")
+    public String managers(){
+        return "Authorized manager";
+    }
+
+
+}
