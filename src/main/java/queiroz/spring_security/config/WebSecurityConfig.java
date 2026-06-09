ffe60bec7e59b17fef2aead655306fcb829949cd
AndersonQueirooz <andersonqueiroz537@gmail.com>
2026-06-09 13:38:48 -0300

    implementando configurações de autenticação com banco de dados

--- src/main/java/queiroz/spring_security/config/WebSecurityConfig.java
+++ src/main/java/queiroz/spring_security/config/WebSecurityConfig.java
@@ -1,36 +1,50 @@
-package queiroz.spring_security;
+package queiroz.spring_security.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import queiroz.spring_security.service.SecurityDatabaseService;
+
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private SecurityDatabaseService securityService;
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception{
-        http.authorizeRequests()
-                .antMatchers("/").permitAll()
-                .antMatchers("/login").permitAll()
-                .antMatchers("/managers").hasAnyRole("MANAGERS")
-                .antMatchers("/users").hasAnyRole("USERS", "MANAGERS")
-                .anyRequest().authenticated().and().formLogin();
+    @Autowired
+    public void globalUserDetails(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(securityService).passwordEncoder(NoOpPasswordEncoder.getInstance());
     }
 
     @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers(HttpMethod.POST,"/login").permitAll()
+                .antMatchers("/managers").hasAnyRole("MANAGERS")
+                .antMatchers("/users").hasAnyRole("USERS","MANAGERS")
+                .anyRequest().authenticated().and().httpBasic();
+    }
+    /*
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
-                .withUser("User")
+                .withUser("user")
                 .password("{noop}user123")
                 .roles("USERS")
                 .and()
                 .withUser("admin")
                 .password("{noop}master123")
-                .roles("Managers");
+                .roles("MANAGERS");
     }
+    */
+
 }
